39b51cee986f7951adbd3409ce607cf6d442bb53
Gagni, Paul <paul.gagni@elevancehealth.com>
2022-11-01 08:57:03 -0400

    Initial event bus created

---
+++ event-bus/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+//Create a new express application
+const app = express();
+//Associate bodyParser middleware with the app
+app.use(bodyParser.json());
+
+//Setup a post request handler
+app.post('/events', (req, res) => {
+    //Get the event that is coming in the request body
+    const event = req.body;
+    //Make post request to running services of the event that is passed
+    axios.post('http://localhost:4000/events', event)
+    axios.post('http://localhost:4001/events', event)
+    axios.post('http://localhost:4002/events', event)
+
+    res.send({ status: 'SENT' });
+});
+
+app.listen(4005, () => {
+    console.log('Listening on 4005');
+});
